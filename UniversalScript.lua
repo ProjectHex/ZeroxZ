c964c92e14afb14d425d8849392563e92d7bd9ad
ProjectHex <62734043+ProjectHex@users.noreply.github.com>
2020-07-12 11:08:41 +0700

    Create UniversalScript.lua

---
+++ UniversalScript.lua
@@ -0,0 +1,181 @@
+_G.CustomTheme = {
+	Tab_Color = Color3.fromRGB(44, 44, 44),
+    Tab_Text_Color = Color3.fromRGB(255, 255, 255),
+    Description_Color = Color3.fromRGB(44, 44, 44),
+    Description_Text_Color = Color3.fromRGB(255, 255, 255),
+    Container_Color = Color3.fromRGB(44, 44, 44),
+    Container_Text_Color = Color3.fromRGB(255, 255, 255),
+    Button_Text_Color = Color3.fromRGB(255, 255, 255),
+    Toggle_Box_Color = Color3.fromRGB(59, 59, 59),
+    Toggle_Inner_Color = Color3.fromRGB(94, 255, 180),
+    Toggle_Text_Color = Color3.fromRGB(255, 255, 255),
+    Toggle_Border_Color = Color3.fromRGB(44, 44, 44),
+    Slider_Bar_Color = Color3.fromRGB(59, 59, 59),
+    Slider_Inner_Color = Color3.fromRGB(94, 255, 180),
+    Slider_Text_Color = Color3.fromRGB(255, 255, 255),
+	Slider_Border_Color = Color3.fromRGB(44, 44, 44),
+    Dropdown_Text_Color = Color3.fromRGB(255, 255, 255),
+    Dropdown_Option_BorderSize = 1,
+    Dropdown_Option_BorderColor = Color3.fromRGB(44, 44, 44),
+    Dropdown_Option_Color = Color3.fromRGB(59, 59, 59),
+    Dropdown_Option_Text_Color = Color3.fromRGB(255, 255, 255)
+}
+repeat wait() 
+	until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
+local mouse = game.Players.LocalPlayer:GetMouse() 
+repeat wait() until mouse
+local plr = game.Players.LocalPlayer 
+local torso = plr.Character.Head 
+local flying = false
+local deb = true 
+local ctrl = {f = 0, b = 0, l = 0, r = 0} 
+local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+local maxspeed = 50 
+local speed = 0 
+flying = false
+
+
+
+function Fly() 
+local bg = Instance.new("BodyGyro", torso) 
+bg.P = 9e4 
+bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
+bg.cframe = torso.CFrame 
+local bv = Instance.new("BodyVelocity", torso) 
+bv.velocity = Vector3.new(0,0.1,0) 
+bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
+repeat wait() 
+plr.Character.Humanoid.PlatformStand = true 
+if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
+speed = speed+.5+(speed/maxspeed) 
+if speed > maxspeed then 
+speed = maxspeed 
+end 
+elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
+speed = speed-1 
+if speed < 0 then 
+speed = 0 
+end 
+end 
+if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
+elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
+bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+else 
+bv.velocity = Vector3.new(0,0.1,0) 
+end 
+bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
+until not flying 
+ctrl = {f = 0, b = 0, l = 0, r = 0} 
+lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+speed = 0 
+bg:Destroy() 
+bv:Destroy() 
+plr.Character.Humanoid.PlatformStand = false 
+end 
+mouse.KeyDown:connect(function(key) 
+if key:lower() == "w" then 
+ctrl.f = 1 
+elseif key:lower() == "s" then 
+ctrl.b = -1 
+elseif key:lower() == "a" then 
+ctrl.l = -1 
+elseif key:lower() == "d" then 
+ctrl.r = 1 
+end 
+end) 
+mouse.KeyUp:connect(function(key) 
+if key:lower() == "w" then 
+ctrl.f = 0 
+elseif key:lower() == "s" then 
+ctrl.b = 0 
+elseif key:lower() == "a" then 
+ctrl.l = 0 
+elseif key:lower() == "d" then 
+ctrl.r = 0 
+end 
+end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/2xDTKdpV", true))()
+
+local Universal = Library:CreateTab("Universal", "This is where you can modify your charater'stuffs")
+
+Universal:CreateSlider("Speed", 0, 1000, function(arg) --the (arg) is the sliders value
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = arg
+end)
+Universal:CreateSlider("JumpPower", 0, 1000, function(arg) --the (arg) is the sliders value
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = arg
+end)
+Universal:CreateToggle("Fly", function(arg) --the (arg) is if the checkbox is toggled or not
+    if arg then
+    flying = true 
+    Fly() 
+
+    else
+     if flying then flying = false end
+    end
+end)
+
+Universal:CreateSlider("Fly Speed", 0, 500, function(arg) --the (arg) is the sliders value
+    maxspeed = arg
+end)
+Universal:CreateButton("NoClip (E To Toggle) ", function() --you dont need "arg" for a button
+    noclip = false
+    game:GetService('RunService').Stepped:connect(function()
+    if noclip then
+    game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+    end
+    end)
+    plr = game.Players.LocalPlayer
+    mouse = plr:GetMouse()
+    mouse.KeyDown:connect(function(key)
+
+    if key == "e" then
+    noclip = not noclip
+    game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+    end
+    end)
+
+end)
+Universal:CreateButton("Respawn (No Spam)", function() --you dont need "arg" for a button
+ 
+local saved = groot.Position
+       
+local ch = game.Players.LocalPlayer.Character
+local prt=Instance.new("Model", workspace)
+local z1 =  Instance.new("Part", prt)
+z1.Name="Torso"
+z1.CanCollide = false
+z1.Anchored = true
+local z2  =Instance.new("Part", prt)
+z2.Name="Head"
+z2.Anchored = true
+z2.CanCollide = false
+local z3 =Instance.new("Humanoid", prt)
+z3.Name="Humanoid"
+z1.Position = Vector3.new(0,9999,0)
+z2.Position = Vector3.new(0,9991,0)
+ game.Players.LocalPlayer.Character=prt
+wait(5)
+ game.Players.LocalPlayer.Character=ch
+local poop = nil
+ repeat wait() poop = game.Players.LocalPlayer.Character:FindFirstChild("Head") until poop ~= nil
+        wait(1)
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(saved)
+       
+ end
+
+end)
